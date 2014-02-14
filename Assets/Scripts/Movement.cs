0709fb26510bb923bfe62ae2bffd899c8d7cedc4
Simon Engelbrecht Sørensen <s.engelbrecht@gmail.com>
2014-02-14 23:19:59 +0100

    controls wut

---
+++ Assets/Scripts/Movement.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Movement : MonoBehaviour {
+
+	private CharacterMotor motor;
+	void Start () 
+	{
+		motor = GetComponent<CharacterMotor>();
+	}
+	
+	
+	void Update () 
+	{
+
+		motor.inputMoveDirection = new Vector3(Vector3.right.x * Input.GetAxis("Horizontal"), 0, Vector3.forward.z * Input.GetAxis("Vertical"));
+		motor.inputJump = Input.GetKey(KeyCode.Space);
+		
+		
+	}
+	
+	void FixedUpdate()
+	{
+		Debug.Log (motor.grounded);
+		
+		
+	}
+}
+
+
+
+
