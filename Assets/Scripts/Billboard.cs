52d561ea247ef5a795bb998ba187160e9bd5443e
Simon Engelbrecht Sørensen <s.engelbrecht@gmail.com>
2014-02-15 15:47:41 +0100

    candleglow

---
+++ Assets/Scripts/Billboard.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Billboard : MonoBehaviour
+{
+	public Camera m_Camera;
+
+	void Start()
+	{
+		m_Camera = Camera.main; 
+	}
+	void Update()
+	{
+		transform.LookAt(transform.position + m_Camera.transform.rotation * Vector3.back,
+		                 m_Camera.transform.rotation * Vector3.up);
+	}
+}
