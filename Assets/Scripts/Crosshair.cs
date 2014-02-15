25c424aa41198150a2eff2c73996fdea4ab3f5b7
Simon Engelbrecht Sørensen <s.engelbrecht@gmail.com>
2014-02-15 01:42:01 +0100

    crosshair navmesh movement

---
+++ Assets/Scripts/Crosshair.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Crosshair : MonoBehaviour {
+	public Texture2D crosshairTexture;
+	public Rect position;
+	public float crosshairSize = 5;
+	// Use this for initialization
+	void Start () 
+	{
+		position = new Rect((Screen.width - crosshairSize) / 2, (Screen.height - crosshairSize) /2, crosshairSize, crosshairSize);
+	}
+	
+
+	
+	void OnGUI()
+	{
+//		if(OriginalOn == true)
+//		{
+			GUI.DrawTexture(position, crosshairTexture);
+//		}
+	}
+	void Update () {
+	
+	}
+}
---
+++ Assets/Scripts/GuestMovement.cs
@@ -0,0 +1,34 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class GuestMovement : MonoBehaviour {
+	public float walkRadius = 20;
+	Vector3 randomDirection;
+	private NavMeshAgent agent;
+	Vector3 finalPosition;
+	private float dist;
+	// Use this for initialization
+	void Start () {
+		agent = GetComponent<NavMeshAgent>();
+		randomDirection = Random.insideUnitSphere * walkRadius;
+		randomDirection += transform.position;
+		NavMeshHit hit;
+		NavMesh.SamplePosition(randomDirection, out hit, walkRadius, 1);
+		finalPosition = hit.position;
+		agent.SetDestination(finalPosition);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		dist = Vector3.Distance(this.transform.position, finalPosition);
+		if(dist < 2f)
+		{
+			randomDirection = Random.insideUnitSphere * walkRadius;
+			randomDirection += transform.position;
+			NavMeshHit hit;
+			NavMesh.SamplePosition(randomDirection, out hit, walkRadius, 1);
+			finalPosition = hit.position;
+			agent.SetDestination(finalPosition);
+		}
+	}
+}
